1799484beb938bc5e6fd0e2916c47aa5b6c3f9e8
HalexRamos <halexramos96@gmail.com>
2021-03-24 14:24:09 -0300

    first

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'e6e9c5f56247d1a9928a1d21b1425072',
+    expiresIn: '1d',
+  },
+};
---
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import SessionsControler from '@modules/users/infra/http/controllers/SessionsController';
+
+const sessionsRouter = Router();
+const sessionsController = new SessionsControler();
+
+sessionsRouter.post('/', sessionsController.create);
+
+export default sessionsRouter;
